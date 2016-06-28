e1722e8f21f5bcfe36f37daf2f32e02af4a27b8b
dougperosa <doug.perosa@gmail.com>
2016-06-28 10:26:06 -0300

    Atualização

---
+++ www/js/controllers.js
@@ -0,0 +1,14 @@
+angular.module('app.controllers', [])
+  
+.controller('onlineCtrl', function($scope) {
+
+})
+   
+.controller('mSicasTocadasCtrl', function($scope) {
+
+})
+   
+.controller('contatoCtrl', function($scope) {
+
+})
+    
